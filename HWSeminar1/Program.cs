bbc9c8678a272178eab9507cbda3e07c3c75fb10
Sount <darkzxc@gmail.com>
2023-09-15 00:01:52 +0300

    update HWSeminar1

--- HWSeminar1/Program.cs
+++ HWSeminar1/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Program 2");
+
+int[] values = new int[2];
+
+Console.WriteLine("Enter two values:");
+values[0] = Convert.ToInt32(Console.ReadLine());
+values[1] = Convert.ToInt32(Console.ReadLine());
+
+if (values[0] > values[1]) 
+    Console.WriteLine($"max is {values[0]}");
+else if (values[1] > values[0]) 
+    Console.WriteLine($"max is {values[1]}");
+else 
+    Console.WriteLine($"{values[0]} = {values[1]}");
+
+Console.WriteLine();
+Console.WriteLine("Program 4");
+
+values = new int[3];
+
+Console.WriteLine("Enter tree values:");
+for(int i = 0; i < values.Length; i++)
+    values[i] = Convert.ToInt32(Console.ReadLine());
+
+int max = values[0];
+for(int i = 0; i < values.Length; i++)
+    if (values[i] > max)
+        max = values[i];
+
+Console.WriteLine($"max is {max}");
+
+Console.WriteLine();
+Console.WriteLine("Program 6");
+
+Console.WriteLine("Enter value:");
+int value = Convert.ToInt32(Console.ReadLine());
+
+if (value % 2 == 0)
+    Console.WriteLine($"{value} is even");
+else 
+    Console.WriteLine($"{value} is uneven");
+
+Console.WriteLine();
+Console.WriteLine("Program 8");
+
+Console.WriteLine("Enter N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 2; i <= n; i += 2)
+    Console.Write($" {i}");
+    
+
+
