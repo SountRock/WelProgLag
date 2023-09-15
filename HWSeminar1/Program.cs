5f828a4c0bcfbf0c5ceff849afdfe038a162d337
Sount <darkzxc@gmail.com>
2023-09-15 10:35:15 +0300

    update HWSeminar1. add repeat program system

--- HWSeminar1/Program.cs
+++ HWSeminar1/Program.cs
@@ -1,53 +1,109 @@
 ﻿Console.WriteLine("Program 2");
 
+bool repeat = true;
+string answer;
+
 int[] values = new int[2];
 
-Console.WriteLine("Enter two values:");
-values[0] = Convert.ToInt32(Console.ReadLine());
-values[1] = Convert.ToInt32(Console.ReadLine());
+while (repeat) 
+{
+    Console.WriteLine("Enter two values:");
+    values[0] = Convert.ToInt32(Console.ReadLine());
+    values[1] = Convert.ToInt32(Console.ReadLine());
 
-if (values[0] > values[1]) 
-    Console.WriteLine($"max is {values[0]}");
-else if (values[1] > values[0]) 
-    Console.WriteLine($"max is {values[1]}");
-else 
-    Console.WriteLine($"{values[0]} = {values[1]}");
+    if (values[0] > values[1]) 
+        Console.WriteLine($"max is {values[0]}");
+    else if (values[1] > values[0]) 
+        Console.WriteLine($"max is {values[1]}");
+    else 
+        Console.WriteLine($"{values[0]} = {values[1]}");
+
+    Console.WriteLine("Repear?(Y/N)");
+    answer = Console.ReadLine();
+    if(answer.ToLower().Trim() == "y"){
+        repeat = true;
+    } else {
+        repeat = false;
+    }
+}
+
+
+repeat = true;
 
 Console.WriteLine();
 Console.WriteLine("Program 4");
 
 values = new int[3];
 
-Console.WriteLine("Enter tree values:");
-for(int i = 0; i < values.Length; i++)
-    values[i] = Convert.ToInt32(Console.ReadLine());
+while (repeat) 
+{
+    Console.WriteLine("Enter tree values:");
+    for(int i = 0; i < values.Length; i++)
+        values[i] = Convert.ToInt32(Console.ReadLine());
 
-int max = values[0];
-for(int i = 0; i < values.Length; i++)
-    if (values[i] > max)
-        max = values[i];
+    int max = values[0];
+    for(int i = 0; i < values.Length; i++)
+        if (values[i] > max)
+            max = values[i];
 
-Console.WriteLine($"max is {max}");
+    Console.WriteLine($"max is {max}");
+
+    Console.WriteLine("Repear?(Y/N)");
+    answer = Console.ReadLine();
+    if(answer.ToLower().Trim() == "y"){
+        repeat = true;
+    } else {
+        repeat = false;
+    }
+}
+
+repeat = true;
 
 Console.WriteLine();
 Console.WriteLine("Program 6");
 
-Console.WriteLine("Enter value:");
-int value = Convert.ToInt32(Console.ReadLine());
+while (repeat) 
+{
+    Console.WriteLine("Enter value:");
+    int value = Convert.ToInt32(Console.ReadLine());
 
-if (value % 2 == 0)
-    Console.WriteLine($"{value} is even");
-else 
-    Console.WriteLine($"{value} is uneven");
+    if (value % 2 == 0)
+        Console.WriteLine($"{value} is even");
+    else 
+        Console.WriteLine($"{value} is uneven");
 
-Console.WriteLine();
-Console.WriteLine("Program 8");
+    Console.WriteLine("Repear?(Y/N)");
+    answer = Console.ReadLine();
+    if(answer.ToLower().Trim() == "y"){
+        repeat = true;
+    } else {
+        repeat = false;
+    }
+}
 
-Console.WriteLine("Enter N:");
-int n = Convert.ToInt32(Console.ReadLine());
+repeat = true;
 
-for(int i = 2; i <= n; i += 2)
-    Console.Write($" {i}");
-    
+while (repeat) 
+{
+    Console.WriteLine();
+    Console.WriteLine("Program 8");
 
+    Console.WriteLine("Enter N:");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    for(int i = 2; i <= n; i += 2)
+        Console.Write($" {i}");
+
+    Console.WriteLine();
+
+    Console.WriteLine("Repear?(Y/N)");
+    answer = Console.ReadLine();
+    if(answer.ToLower().Trim() == "y"){
+        repeat = true;
+    } else {
+        repeat = false;
+    }
+}   
+
+Console.WriteLine("Goodbye");
 
